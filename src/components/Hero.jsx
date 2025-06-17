2f3eaa0683777f7c50c9d674f7de34cf1ca8dd2d
Roshani Sahu <roshani032003@gmail.com>
2025-06-17 09:03:09 +0530

    "third"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ export default function Hero() {
       
  <div className="absolute inset-0 bg-black bg-opacity-30"></div>
   
-      <div className="absolute -bottom-14 right-0 left-0  z-10 max-w-6xl mx-auto px-4 text-center text-white pt-24">
-        <h1 className="text-4xl md:text-5xl font-bold">
+      <div className="absolute -bottom-16 right-0 left-0  z-10 max-w-6xl mx-auto px-4 text-center text-white pt-24">
+        <h1 className="text-5xl md:text-5xl font-semibold">
           Plan Your Dream <span className="text-pink-500">Wedding</span> – All in One Place
         </h1>
         <p className="mt-4  text-lg md:text-xl text-gray-200">
@@ -16,9 +16,9 @@ export default function Hero() {
         </p>
 
         
-        <div className="mt-10 h-[150px] w-[1060px] bg-white rounded-2xl shadow-lg p-4 md:p-6 flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 max-w-4xl mx-auto">
+        <div className="mt-10 h-[260px] w-[590px] md:h-[150px] md:w-[1060px] bg-white rounded-2xl shadow-lg p-6 md:p-6 flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 max-w-4xl mx-auto">
           <div className="flex-1 w-full">
-  <select className="w-[311px] h-[61px] border rounded-lg p-2 text-gray-500 focus:outline-pink-500">
+  <select className=" w-[551px] h-[61px] md:w-[311px] md:h-[61px] border rounded-lg p-2 text-gray-500 focus:outline-pink-500">
     <option value="" disabled selected hidden>
       Search for Vendors
     </option>
@@ -29,7 +29,7 @@ export default function Hero() {
 </div>
 
          <div className="flex-1 w-full">
-  <select className="w-[311px] h-[61px] border rounded-lg p-2 bg-white text-gray-500 focus:outline-pink-500">
+  <select className="w-[551px] h-[61px] md:w-[311px] md:h-[61px] border rounded-lg p-2 bg-white text-gray-500 focus:outline-pink-500">
     <option value="" disabled selected hidden>
       Search Location
     </option>
@@ -39,7 +39,7 @@ export default function Hero() {
   </select>
 </div>
 
-          <button className="bg-[#EF4A6B] hover:bg-pink-600 text-white font-semibold px-6 py-3 w-[254px] h-[60px] rounded-lg whitespace-nowrap">
+          <button className="bg-[#EF4A6B] hover:bg-pink-600 text-white font-semibold px-6 py-3 w-[550px] h-[60px] md:w-[254px] md:h-[60px] rounded-lg whitespace-nowrap">
             Find Vendors
           </button>
         </div>
